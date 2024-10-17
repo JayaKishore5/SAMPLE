3368afa5bd63e3539c0b203a8e86bc6bc8d6fb9f
JayaKishore535 <jayakishore7077@gmail.com>
2024-10-17 21:44:32 +0530

    Commit 2

--- src/main/java/com/example/SAMPLE/controller/HelloController.java
+++ src/main/java/com/example/SAMPLE/controller/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.SAMPLE.controller;
 
 
+import com.example.SAMPLE.entity.Message;
+import com.example.SAMPLE.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("hello")
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello!";
+        // Save the message to the database
+        Message message = new Message("Hello!");
+        messageRepository.save(message);
+
+        return "Message saved: " + message.getContent();
     }
 
 }
